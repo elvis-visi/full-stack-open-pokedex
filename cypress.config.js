12e145d5956109a9723215a0c67972ae29d104a1
elvis-visi <sivisevi1@gmail.com>
2024-03-17 18:14:13 +0100

    added the supportFile: false line under the e2e configuration object

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,9 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents( ) {
+    setupNodeEvents() {
       // implement node event listeners here
     },
+    supportFile: false,
   },
 })
